85c8176683423395720ce627957bc9a5bacde472
Divyansi Mishra <divyansim01@gmail.com>
2022-10-12 08:57:14 +0530

    Updates

---
+++ precache-manifest.01f62c06b20fc86d88ae810d659a48e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58fe39674b2f47cef397",
+    "url": "/ReactJs/static/css/main.3dc02ab9.chunk.css"
+  },
+  {
+    "revision": "58fe39674b2f47cef397",
+    "url": "/ReactJs/static/js/main.58fe3967.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/ReactJs/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/ReactJs/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "39d314f54fd791340047",
+    "url": "/ReactJs/static/js/runtime~main.39d314f5.js"
+  },
+  {
+    "revision": "b39418805757ff8c7ce03f2df00b8d58",
+    "url": "/ReactJs/index.html"
+  }
+];
