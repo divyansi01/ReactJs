ee4a4e2d9f4f85066ab056c7d50508fc3583d68a
Divyansi Mishra <divyansim01@gmail.com>
2022-10-12 09:03:33 +0530

    new

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+// import { a } from 'react-router-dom'
 
 export default function Navbar(props) {
     let myStyle = {
@@ -14,18 +14,18 @@ export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <Link className={`navbar-brand text-${props.mode === 'light' ? 'dark' : 'light'}`} to="/" style={myStyle}>{props.title}</Link>
+                <a className={`navbar-brand text-${props.mode === 'light' ? 'dark' : 'light'}`} href="#" style={myStyle}>{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item" >
-                            <Link className={`nav-link active text-${props.mode === 'light' ? 'dark' : 'light'}`} style={myStyle} aria-current="page" to="/">Home</Link>
+                            <a className={`nav-a active text-${props.mode === 'light' ? 'dark' : 'light'}`} style={myStyle} aria-current="page" href="#">Home</a>
                         </li>
-                        <li className="nav-item">
-                            <Link className={`nav-link active text-${props.mode === 'light' ? 'dark' : 'light'}`} to="/about" style={myStyle}>About</Link>
-                        </li>
+                        {/* <li className="nav-item">
+                            <a className={`nav-a active text-${props.mode === 'light' ? 'dark' : 'light'}`} href="/about" style={myStyle}>About</a>
+                        </li> */}
                     </ul>
                     {/* <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
